d2dba2e91f1c97af8245d3dee9212953b25e7a54
Mike Alexander <mikealexander1860@gmail.com>
2018-06-19 11:02:46 -0700

    Moved test server configuration to a separate module so it can be used in multiple tests

---
+++ v1pysdk/tests/common_test_server.py
@@ -0,0 +1,11 @@
+# Simply defines the common 
+
+class PublicTestServerConnection():
+    username = 'admin'
+    password = 'admin'
+    address = 'www14.v1host.com'
+    instance = 'v1sdktesting'
+    #TODO: Add OAuth key
+
+    def __init__():
+        pass
--- v1pysdk/tests/connect_tests.py
+++ v1pysdk/tests/connect_tests.py
@@ -11,10 +11,22 @@ except ImportError:
     from elementtree.ElementTree import parse, fromstring, Element
 
 from v1pysdk.client import *
+from .common_test_server import PublicTestServerConnection
 
 class TestV1Connection(TestCase):
-  def test_connect(self, username='admin', password='admin'):
-    server = V1Server(address='www14.v1host.com', username=username, password=password,instance='v1sdktesting')
+  def test_connect(self, username=None, password=None, address=None, instance=None):
+    if not username and not password:
+        username = PublicTestServerConnection.username
+        password = PublicTestServerConnection.password
+    if not address:
+        address = PublicTestServerConnection.address
+    if not instance:
+        instance = PublicTestServerConnection.instance
+    print("")
+    print("Using Server: " + address + "/" + instance + ", User=" + username)
+    
+    server = V1Server(address=address, username=username, password=password,instance=instance)
+    # The story names, but limit to only the first result so we don't get inundated with results
     code, body = server.fetch('/rest-1.v1/Data/Story?sel=Name&page=1,0')
     print("\n\nCode: ", code)
     print("Body: ", body)
